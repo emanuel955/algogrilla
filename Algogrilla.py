be69a75b06ed60d6555461bf961395ebdebb6945
emanuel955 <emanuelzetka@gmail.com>
2018-10-24 22:58:25 -0300

    arreglos

--- Algogrilla.py
+++ Algogrilla.py
@@ -4,11 +4,12 @@ import random
 def main():
 	
 	frase_aleatoria = elegir_frase('frases.csv')
-	palabras_aleatorias('palabras.csv', frase_aleatoria)
+	elegir_palabras('palabras.csv', frase_aleatoria)
 
 
-def palabras_aleatorias(arc_palabras, frase_aleatoria):
-	'''recibe un archivo csv con palabras|silabas|definicion, devuelve una lista con palabras al azar'''
+def elegir_palabras(arc_palabras, frase_aleatoria):
+	'''recibe un archivo csv con palabras|silabas|definicion y una lista que contiene una frase,columnas,autor
+	y devuelve una lista con palabras al azar'''
 	listFrase = partir_frase(frase_aleatoria)
 	print(len(listFrase[0]), len(listFrase[1]))
 	palabras = abrirArchivo(arc_palabras)
@@ -16,17 +17,35 @@ def palabras_aleatorias(arc_palabras, frase_aleatoria):
 	listaPalabra = []
 	listaDePalabras = list(palabras.keys())
 	numPosicion = frase_aleatoria[1].split(',') #lista que contiene las columnas
-	print(numPosicion)
-	'''while len(listaPalabra) <= max(len(listFrase[0],len(listFrase[1]))): 
-		palabraAlea = random.choice(listaDePalabras)
-		if '''
-
+	maximo = max(len(listFrase[0]),len(listFrase[1]))
+	for i in range(maximo):
+		for c in listaDePalabras:
+			if c in listaPalabra:
+				continue
+			if len(listFrase[0]) > len(listFrase[1]) and i == (maximo - 1):
+				if len(c) < len(numPos[1]) and c[int(numPos[0])] == listFrase[0][i]:
+					listaPalabra.append(c)
+					break
+				continue
+			if c[int(numPos[0])] == listFrase[0][i] and c[int(numPos[1])] == listFrase[1][i]:
+				listaPalabra.append(c)
+				break
+	print(listaPalabra)
+	return listaPalabra
 def partir_frase(frase):
-	'''recibe una lista que contiene la frase en la posicion cero, devuelve una lista con la frase partida en dos'''
-	print(''.join(frase[0].split()))
-	fraseJunta = ''.join(frase[0].split())
-	print([fraseJunta[:len(fraseJunta)//2],fraseJunta[(len(fraseJunta)//2):]])
-	return [fraseJunta[:len(fraseJunta)//2],fraseJunta[(len(fraseJunta)//2):]]
+	'''recibe una lista que contiene la frase en la posicion cero, devuelve una lista con la frase partida en dos
+	si la longitud de la cadena es impar la primera parte sera mas grande que la segunda'''
+	fraseJunta = ''
+	for i in frase[0]:
+		if not i.isalpha():
+			continue
+		fraseJunta += i
+	print(fraseJunta, len(fraseJunta))
+	if len(fraseJunta)%2 !=0:
+		return [fraseJunta[:(len(fraseJunta)//2)+1],fraseJunta[(len(fraseJunta)//2)+1:]]
+
+	print([fraseJunta[:(len(fraseJunta)//2)],fraseJunta[(len(fraseJunta)//2):]])
+	return [fraseJunta[:(len(fraseJunta)//2)],fraseJunta[(len(fraseJunta)//2):]]
 
 	
 def elegir_frase(arc_frases):
@@ -37,8 +56,8 @@ def elegir_frase(arc_frases):
 	listaClaves = list(frases.keys()) #toma las claves del dicc y las guarda en una lista
 	claveAlea = random.choice(listaClaves)
 	valorClaveAlea = frases.get(claveAlea)
-	print([claveAlea.strip('"'), valorClaveAlea[0], valorClaveAlea[1]])
-	return [claveAlea.strip('"'), valorClaveAlea[0], valorClaveAlea[1]]
+	print([claveAlea.strip('"').lower(), valorClaveAlea[0], valorClaveAlea[1]])
+	return [claveAlea.strip('"').lower(), valorClaveAlea[0], valorClaveAlea[1]]
 
 def abrirArchivo(archivo):
 	'''recibe un archivo y devuelve el contenido en un diccionario'''
